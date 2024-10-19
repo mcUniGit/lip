c2616bb8d28ae8309d7e32533a04817b65d3c380
m <mau@MacBook-Air-di-maurizio.local>
2024-10-19 16:28:29 +0200

    Added functions in basics exercises

--- basics/recognizer/lib/recognizer.ml
+++ basics/recognizer/lib/recognizer.ml
@@ -1,14 +1,40 @@
-let lang1 _ = failwith ""
+let rec lang1 = function  
+ |['0']-> true
+ |['1'] -> true
+ |'0'::l -> lang1 l 
+ |'1'::l -> lang1 l 
+ |_ -> false;;   
 
-let lang2 _ = failwith ""
+let rec lang2 = function
+  [] |['0'] |['1'] -> true
+ |'0'::l -> lang2 l 
+ |'1'::l -> lang2 l 
+ |_ -> false
 
-let lang3 _ = failwith ""
+let lang3 l = match l with
+    [] -> false
+    |'0'::_ ->
+            let rec check = function
+                '0'::['0'] |'1'::['0'] -> true 
+                |'0'::l |'1'::l -> check l
+                |_ -> false in check l
+    |_ -> false
 
-let lang4 _ = failwith ""
+let lang4 l =  
+    let rec check l o = match l with
+        [] -> o > 1
+        |'0'::l -> check l o
+        |'1'::l -> check l (o+1) 
+        |_ -> false in check l 0 
 
-let lang5 _ = failwith ""
-    
+let rec lang5 = function 
+    ['0';'0'] |['1';'1'] -> true
+    |'0'::'0'::l | '1'::'1'::l -> lang5 l 
+    |_ -> false
+
 let recognizers = [lang1;lang2;lang3;lang4;lang5]
                   
-let belongsTo w = List.map (fun f -> f w) recognizers
+let belongsTo w = List.map (fun f -> f w) recognizers;;
+
+belongsTo ['1';'0';'0';'1'];; 
   
--- basics/tugofwar/lib/tugofwar.ml
+++ basics/tugofwar/lib/tugofwar.ml
@@ -1,3 +1,4 @@
+
 (* tokens *)
 type token = A | B | X
 
@@ -5,17 +6,41 @@ type token = A | B | X
 (* toklist_of_string s transforms the string s into a list of tokens *)
 (* Hint: use the function explode in bin/main.ml to convert a string to a char list *)
              
-let toklist_of_string s = failwith "TODO"
+let toklist_of_string s = 
+  let explode s =
+    let rec exp i l =
+      if i < 0 then l else exp (i - 1) (s.[i] :: l) in
+    exp (String.length s - 1) [] in
+
+  let tok_of_char c = match c with
+      'A' -> A
+    |'B' -> B
+    |'=' -> X  
+    |_ -> failwith "Wrong input" in
+
+  List.map tok_of_char (explode s)  
 
 (* val valid : token list -> bool *)
 (* valid l is true when l is a list of tokens in the language A* X* B* *)
+
+let valid l =
     
-let valid l = failwith "TODO"
+  let rec check x b l = match l with
+      [] -> true
+    |A::l -> not(x || b) && check x b l 
+    |X::l -> (not b) && check true b l 
+    |B::l -> check x true l in check false false l 
 
 (* val win : token list -> token *)
 (* win l determines the winner of a tug of war game. X means tie *)
+let win l = 
+  let winner = List.fold_left( fun acc t -> if t = A then acc - 1 else if t = B then acc + 1 else acc ) 0 l in
 
-let win l = failwith "TODO"
+  if winner < 0 then A else if winner = 0 then X else B
 
 (* val string_of_winner : token -> string *)
-let string_of_winner w = failwith "TODO"
+let string_of_winner w = match w with
+    X -> "tie"
+  |A -> "A"
+  |B -> "B";;
+
